7caa251e1a41d3e9b06933186cbb6310d906d6d9
alius <alius@LAPTOP-NNR66SG1>
2021-03-15 20:43:19 +0100

    created model  and form folder

---
+++ Asp.net-Assignment/Properties/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_Assignment.Properties.Models
+{
+    public class Model
+    {
+    }
+}
